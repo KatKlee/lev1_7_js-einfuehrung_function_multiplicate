3b78aadced36ff9a1b8c0d7c8746db1a535b6618
KatKlee <katklee2022@gmail.com>
2022-07-28 16:30:40 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+function multi(numA) {
+    return numA * 2
+}
+
+console.log(multi(5))
+/* 10 */
+
+console.log(multi(15))
+/* 30 */
+
+console.log(multi(20))
+/* 40 */
+
+console.log(multi(120))
+/* 240 */
